c78984099689f12154e7c6b74dbbeb267808fc0f
KayKamakshiGupta <kamakshig2310@gmail.com>
2024-02-11 00:12:28 +0530

    updated navbar

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -6,13 +6,28 @@ export default function App(){
     return (
     <Html lang="en">
       <Head />
+      <style>
+                {`
+          .list{
+            gap: 60px;
+          }
+                `}
+           
+          
+            </style>
       <body>
+        
         <div className="flex h-20 bg-slate-800 justify-center items-center fixed w-full opacity-70">
             <Link className="mr-auto pl-10" href="/">
                 <img src="/logo.png" alt="Logo" className="h-14" />
             </Link>
-
-        <Link className="mr-auto text-3xl" href='/login'>Hello</Link>
+        <div className='navbar flex mr-auto text-3xl text-white'>
+          <ul className='list flex'>
+            <li><a href='add_crop'>Add Data</a></li>
+            <li><a href='support'>Support</a></li>
+          </ul>
+        </div>
+        {/* <Link className="mr-auto text-3xl" href='/login'>Hello</Link> */}
 
             <div className="justify-end items-center flex gap-10 pr-10 ">
               <a href="/login" className="text-white">Login</a>
