c809fcf0b46d81b068eee1e8e21bd3bbc91a74c4
ShubhamAggarwal6105 <59225025+ShubhamAggarwal6105@users.noreply.github.com>
2024-02-10 16:06:41 +0530

    PROJECT SETUP

---
+++ backend/views/__init__.py
@@ -0,0 +1,16 @@
+"""
+This file contains the route handlers for the Django app.
+"""
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .server import *
+from .creation import *
+from .profile import *
+from .chat import *
+
+def index(_):
+    """
+    Index route handler. Returns a simple JSON response.
+    """
+    return JsonResponse({'message': 'Hello, world!'})
